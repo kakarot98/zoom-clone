d80bef5143ad831c3f5a1ca99cf8534f4226463e
Hrushikesh Mohanty <hrushikeshmohanty90@gmail.com>
2021-05-20 16:04:41 +0530

    making changes again

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const express = require("express");
-const app = express();
-const server = require("http").Server(app);
-const { v4:uuidv4 } = require('uuid')
-app.set("view engine", "ejs");
+const express = require('express')
+app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidv4 } = require('uuid')
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
-
-app.get("/", (req, res) => {
-  res.redirect(`/${uuidv4()}`)
-});
+app.get('/', (req,res)=>{
+res.redirect(`/${uuidv4()}`)
+})
 
 
 app.get('/:room', (req,res)=>{
@@ -16,4 +17,4 @@ app.get('/:room', (req,res)=>{
 })
 
 
-server.listen(3030);
+server.listen(3000)
